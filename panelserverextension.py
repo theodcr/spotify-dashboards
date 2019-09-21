0f526d0012753d53190fc9189cb225821fd25ab1
Théo DELECOUR <theo.delecour@gmx.com>
2019-09-21 20:46:27 +0200

    Adapt server extension

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
-    """serve the gapminder.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "gapminder.ipynb", "--allow-websocket-origin=*"])
+    """serve the notebook with bokeh server"""
+    Popen(["panel", "serve", "1_panel_bokeh_dashboard.ipynb", "--allow-websocket-origin=*"])
